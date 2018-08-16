7b29438e1f4b66bd3aeab025f97d52c9bcae6917
Li-Hang Lin <lihang.lin@gmail.com>
2018-08-16 09:39:16 +0800

    emacs: ggtags: remap key binding
    * remap keybinding M-n/p (ggtags-prev/next-mark)
      to M->/< to avoid conflict with tabbar

--- .emacs.d/tweak/ggtags.el
+++ .emacs.d/tweak/ggtags.el
@@ -10,4 +10,11 @@
 	    (when (derived-mode-p 'c-mode 'c++-mode 'asm-mode)
 	      (ggtags-mode 1))))
 
-(global-set-key (kbd "M-,") 'pop-tag-mark)
+;;(global-set-key (kbd "M-,") 'pop-tag-mark)
+
+;; the original bindkey of M-n and M-p in ggtags
+;; were used for moving to next/prev match
+;; They were conflicted with my tabbar.
+;; I remap both keys to M-> and M-<
+(global-set-key (kbd "M-<") 'ggtags-prev-mark)
+(global-set-key (kbd "M->") 'ggtags-next-mark)
